969b2131de7b6844e5266ff9400a00aeb6c5a1c1
Sumit Kumar Maitra <sumitkm@gmail.com>
2019-10-06 14:19:53 +0100

    More parts

--- src/prototypes/dn_sus.py
+++ src/prototypes/dn_sus.py
@@ -5,10 +5,9 @@ from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
 min = 0
-max = 145
-kit.servo[0].angle = min
-kit.servo[1].angle = min
+max = 45
+kit.servo[9].angle = min
+kit.servo[8].angle = min
 time.sleep(2)
-kit.servo[0].angle = max
-kit.servo[1].angle = max
- 
+kit.servo[9].angle = max
+kit.servo[8].angle = max
